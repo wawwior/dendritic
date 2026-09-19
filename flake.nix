5109fb4a28377018eeb3415d5dac14a1a51d03a7
wawwior <45405580+wawwior@users.noreply.github.com>
2026-09-19 03:15:46 +0200

    fix: remove nixpkgs input left over from testing

--- flake.nix
+++ flake.nix
@@ -3,12 +3,8 @@
 
   inputs = {
     flake-aspects.url = "github:denful/flake-aspects";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:denful/import-tree";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
